7d63c50c98040a9d170b3de922ad308c21ac49c3
Christopher Goddard <chrisjgoddard@gmail.com>
2021-11-03 06:56:52 +1100

    Add remote sync and post output node

---
+++ nodes/outputs/remote_sync_and_post.gd
@@ -0,0 +1,26 @@
+extends ProtonNode
+
+
+func _init() -> void:
+	unique_id = "remote_sync_and_post"
+	display_name = "Remote Sync and Post"
+	category = "Output"
+	description = "Sends the data to a remote program like a game engine or anything else."
+	# input = [{child_transversal:[fence_planks, tmpParent], remote_resource_path:res://assets/fences/models/fence_planks.glb}]
+	set_input(0, "3D Object References", DataType.NODE_3D_REFERENCE)
+	set_input(1, "3D Objects", DataType.NODE_3D)
+	enable_multiple_connections_on_slot(0)
+
+
+func _generate_outputs() -> void:
+	print("in the remote_sync _generate_outputs function")
+	output[0] = { "resources": get_input(0), "nodes": get_input(1) }
+	print(output[0])
+
+
+func is_final_output_node() -> bool:
+	return true
+
+
+func is_remote_sync_node() -> bool:
+	return true
