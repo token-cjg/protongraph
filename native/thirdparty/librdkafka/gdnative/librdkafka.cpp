6453e3b753c2251cae4d91a0e54153d6c466d64a
Christopher Goddard <chrisjgoddard@gmail.com>
2021-12-01 04:52:17 +1100

    Add librdkafka

---
+++ native/thirdparty/librdkafka/gdnative/librdkafka.cpp
@@ -0,0 +1,18 @@
+#include "librdkafka.h"
+#include "../lib/src-cpp/rdkafkacpp.h"
+#include <string>
+#include <vector>
+
+using namespace godot;
+
+void LibRdKafka::_register_methods() {
+  // register_method("optimize", &MeshOptimizer::optimize);
+  // register_method("optimize_mesh", &MeshOptimizer::optimize_mesh);
+  // register_method("optimize_mesh_instance", &MeshOptimizer::optimize_mesh_instance);
+  // register_method("simplify", &MeshOptimizer::simplify);
+}
+
+LibRdKafka::LibRdKafka() {}
+
+LibRdKafka::~LibRdKafka() {}
+
---
+++ native/thirdparty/librdkafka/gdnative/librdkafka.h
@@ -0,0 +1,28 @@
+#ifndef MESH_OPTIMIZER_H
+#define MESH_OPTIMIZER_H
+
+#include <Godot.hpp>
+#include <Sprite.hpp>
+#include <Node.hpp>
+#include <MeshInstance.hpp>
+#include <Transform.hpp>
+#include <NodePath.hpp>
+#include <Mesh.hpp>
+#include <Skin.hpp>
+#include <ArrayMesh.hpp>
+
+namespace godot {
+
+class LibRdKafka : public Reference {
+    GODOT_CLASS(LibRdKafka, Reference);
+
+public:
+    void _init();
+    static void _register_methods();
+     LibRdKafka();
+    ~LibRdKafka();
+};
+
+}
+
+#endif
