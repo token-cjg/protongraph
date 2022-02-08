900cb4117b991f97e5e39729c9c0e3ef87e06d15
Christopher Goddard <chrisjgoddard@gmail.com>
2022-02-09 05:44:17 +1100

    WIP

--- native/compile_all.sh
+++ native/compile_all.sh
@@ -21,13 +21,13 @@ fi
 
 
 
-cd godot-cpp
-scons -j${threads} bits=64 generate_bindings=yes debug_symbols=no platform=$platform target=$target
+# cd godot-cpp
+# scons -j${threads} bits=64 generate_bindings=yes debug_symbols=no platform=$platform target=$target
 
 # Make sure that we add the mesh optimizer.  nb, not sure that this is required any more? https://github.com/godotengine/godot/pull/47764 , https://github.com/protongraph/protongraph/issues/101
-cd ../thirdparty/mesh_optimizer
-scons -j${threads} platform=$platform bits=64 debug_symbols=no target=$target
+# cd ../thirdparty/mesh_optimizer
+# scons -j${threads} platform=$platform bits=64 debug_symbols=no target=$target
 
 # Add kafka client support so that we can consume and produce to kafka topics on a (secured) kafka cluster or vm.
-cd ../librdkafka
+cd thirdparty/librdkafka
 scons -j${threads} platform=$platform bits=64 debug_symbols=no target=$target
