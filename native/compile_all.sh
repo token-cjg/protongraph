38ea601b9438a489c6311450649ba584bca3c6c9
Christopher Goddard <chrisjgoddard@gmail.com>
2022-03-22 06:41:44 +1100

    WIP

--- native/compile_all.sh
+++ native/compile_all.sh
@@ -30,8 +30,16 @@ scons -j${threads} platform=$platform bits=64 debug_symbols=no target=$target
 # We'll need to repoint a dependency within the dynamic library to point to the relative path to wherever we are
 # executing the project.  At present I have set this relative to the root.  When eventually this is compiled to an
 # osx bundle we'll need to change this to be relative to the root of the bundle.
-cp lib/src/librdkafka.1.dylib bin/osx/librdkafka.1.dylib
-install_name_tool -change /usr/local/lib/librdkafka.1.dylib @executable_path/native/thirdparty/librdkafka/bin/osx/librdkafka.1.dylib bin/osx/librdkafka.dylib
+if [ $platform=="osx" ]
+  then
+    cp lib/src/librdkafka.1.dylib bin/osx/librdkafka.1.dylib
+    install_name_tool -change /usr/local/lib/librdkafka.1.dylib @executable_path/native/thirdparty/librdkafka/bin/osx/librdkafka.1.dylib bin/osx/librdkafka.dylib
+fi
+
+if [ $platform=="linux" ]
+  then
+    cp lib/src/librdkafka.so bin/linux/librdkafka.so
+fi
 
 # Make sure that we add the mesh optimizer.  nb, not sure that this is required any more? https://github.com/godotengine/godot/pull/47764 , https://github.com/protongraph/protongraph/issues/101
 cd ../mesh_optimizer
