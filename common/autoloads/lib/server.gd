5f683ac6326ac23a3c830cec43d266ffe9278843
Christopher Goddard <chrisjgoddard@gmail.com>
2022-04-17 16:28:46 +1000

    WIP

--- common/autoloads/lib/server.gd
+++ common/autoloads/lib/server.gd
@@ -127,6 +127,7 @@ func _on_data_received(client_id: int) -> void:
 	# Decode once we've received all the chunks
 	if _incoming[packet_id].size() == total_chunks:
 		print("Received all chunks for packet ", packet_id)
+		_ws.start() # Restart the server to receive more data and also notify the provider that the message has been received.
 		var instanceServiceId: int
 		var instanceId: String
 		var peerKey: String
@@ -163,8 +164,6 @@ func _decode(packet_id: int, client_id: int) -> void:
 	if _incoming[packet_id].has("metadata"):
 		data["metadata"] = _incoming[packet_id]["metadata"]
 	emit_signal("data_received", client_id, data)
-	_ws.close_peer(client_id)
-
 
 func _on_client_close_request(id: int, code: int, reason: String) -> void:
 	print("Client close request ", id, " reason: ", reason)
